8030d7b5fed63714d1cadcbe66290124713cd29d
LVictoria94 <145370124+LVictoria94@users.noreply.github.com>
2023-09-21 18:16:08 +0200

    fixed snake control

--- SnakeGame/Snake.cs
+++ SnakeGame/Snake.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Runtime.Intrinsics;
 using System.Security.Cryptography.Pkcs;
+using System.Windows;
 
 namespace SnakeGame
 {
@@ -11,6 +13,7 @@ namespace SnakeGame
         private readonly Vector2D PERIODICITY;
 
         public Vector2D direction = new Vector2D(1, 0);
+        public String strDirection = "D"; //D=Destra, A=Alto, G=Giu, S=Sinistra -> Parte da destra
 
         public Snake(int row_num, int col_num)
         {
@@ -19,11 +22,25 @@ namespace SnakeGame
             {   this.position.Add(new Vector2D(i, 2)); }
         }
 
-        public void move()
+        
+        public void move(Vector2D direction)
         {
+            this.direction = direction;
             Vector2D head = position[position.Count - 1];
             this.position.Add( (head + direction) % PERIODICITY);
+
+            for(int i=0;i<this.position.Count;i++) {
+                for(int j=0;j<this.position.Count;j++) {
+                    if(i!=j) {
+                        if(this.position[i].x == this.position[j].x && this.position[i].y == this.position[j].y) {
+                            System.Environment.Exit(0);
+                        }
+                    }
+                }
+            }
+
             this.position.RemoveAt(0);
+
         }
 
     }
